a53687ff31451a16161656d71afd7a0f9cee46af
realstoman <realstoman@gmail.com>
2022-08-31 16:34:09 +0430

    Create home screen and initial ui

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0x1B2A4A),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          iconSize: 30.0,
+          color: Colors.white,
+          onPressed: () {},
+        ),
+        title: Text('Stoman',
+            style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold)),
+        elevation: 0.0,
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            iconSize: 30.0,
+            color: Colors.white,
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
