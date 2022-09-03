57956069d76ded07c358b5880e9e263e90994613
realstoman <realstoman@gmail.com>
2022-09-03 16:37:38 +0430

    Update svg links

--- lib/widgets/app_banner.dart
+++ lib/widgets/app_banner.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter_svg/svg.dart';
 
 class AppBanner extends StatelessWidget {
   const AppBanner({Key? key}) : super(key: key);
@@ -10,12 +11,18 @@ class AppBanner extends StatelessWidget {
       child: Container(
           child: Column(
         children: const <Widget>[
-          CircleAvatar(
-            radius: 5.0,
-            // backgroundImage: AssetImage('/learning.svg'),
-            // child: SvgImage('assets/images/learning.svg'),
-          ),
-          SizedBox(height: 6.0),
+          // CircleAvatar(
+          //   radius: 5.0,
+          //   // backgroundImage: AssetImage('/learning.svg'),
+          //   // child: SvgImage('assets/images/learning.svg'),
+          // ),
+          // SizedBox(
+          //   height: 100,
+          //   width: 100,
+          //   child: SvgPicture.asset("assets/flag.svg", //asset location
+          //       color: Colors.red, //svg color
+          //       semanticsLabel: 'SVG From asset folder.'),
+          // ),
           Text('Hello'),
         ],
       )),
