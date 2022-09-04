9d37a60d52666e2ae6bb9d05ad0551d34f14536d
realstoman <realstoman@gmail.com>
2022-09-04 13:41:44 +0430

    Add ap home widget

---
+++ lib/widgets/app_home_buttons.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppHomeButtons extends StatefulWidget {
+  AppHomeButtons({Key? key}) : super(key: key);
+
+  @override
+  State<AppHomeButtons> createState() => _AppHomeButtonsState();
+}
+
+class _AppHomeButtonsState extends State<AppHomeButtons> {
+  late var myName = 'Stoman';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
