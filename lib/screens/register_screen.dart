13785189186a29865ed591f63ced7b1b7b5e5481
realstoman <realstoman@gmail.com>
2022-09-09 16:35:15 +0430

    Add register scaffold

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -8,8 +8,24 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  // Custom color
+  static const customBgColor = Color.fromARGB(255, 27, 42, 74);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: customBgColor,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: const Padding(
+          padding: EdgeInsets.only(
+            left: 10.0,
+            top: 33.0,
+          ),
+        ),
+        elevation: 0.0,
+        centerTitle: false,
+      ),
+    );
   }
 }
