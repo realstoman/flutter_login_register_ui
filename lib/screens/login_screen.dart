214f14bbe512b9d749d506c176aa0db4d7c450a9
realstoman <realstoman@gmail.com>
2022-09-11 10:12:18 +0430

    Create email widget and email input field

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -11,6 +11,31 @@ class _LoginScreenState extends State<LoginScreen> {
   // Custom color
   static const customBgColor = Color.fromARGB(255, 27, 42, 74);
 
+  Widget _buildEmailTF() {
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const Text('Email',
+              style: TextStyle(color: Colors.white, fontFamily: 'GeneralSans')),
+          const SizedBox(height: 10.0),
+          Container(
+            alignment: Alignment.centerLeft,
+            decoration: const BoxDecoration(),
+            height: 60.0,
+            child: const TextField(
+              keyboardType: TextInputType.emailAddress,
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(top: 14.0),
+                prefixIcon: Icon(Icons.email, color: Colors.white),
+                hintText: 'Enter your email',
+              ),
+            ),
+          ),
+        ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,18 +51,31 @@ class _LoginScreenState extends State<LoginScreen> {
         elevation: 0.0,
         centerTitle: false,
       ),
-      body: Column(children: const <Widget>[
-        Padding(
-          padding: EdgeInsets.all(14.0),
-          child: Text('Login to your account',
-              style: TextStyle(
-                color: Colors.white,
-                fontFamily: 'Arial',
-                fontWeight: FontWeight.w500,
-                fontSize: 23,
-              )),
-        ),
-      ]),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: double.infinity,
+            child: SingleChildScrollView(
+              physics: const AlwaysScrollableScrollPhysics(),
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 40.0, vertical: 120.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text('Sign In',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'GeneralSans',
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.w500)),
+                  const SizedBox(height: 30.0),
+                  _buildEmailTF(),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
